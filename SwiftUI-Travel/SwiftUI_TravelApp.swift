724c2bdb1d8c22df5b58d1f4d8e0eafd905d7f8f
SreytouchLang <atouch.it@gmail.com>
2023-12-29 08:53:22 -0800

    Initial Commit

---
+++ SwiftUI-Travel/SwiftUI_TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TravelApp.swift
+//  SwiftUI-Travel
+//
+//  Created by Sreytouch(Jessica) on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
